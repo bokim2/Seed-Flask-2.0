d9fdc993caa9fa13e575d4dc7899f4f3777e34a4
bokim2 <bokim.ce@gmail.com>
2024-01-09 22:15:02 -0800

    route test3

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,37 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 // Remove the import statements for useEffect, useState, and axios
 
-function App() {  
-// eslint-disable-next-line
-const [flask, setFlask] = useState({});
+function App() {
+  // eslint-disable-next-line
+  const [flask, setFlask] = useState({});
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const res = await axios.get('/api/flasks/1');
-      console.log('Axios response:', res);
-      console.log('Data in useEffect:', res.data);
-      setFlask(res.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const res = await axios.get('http://localhost:3000/api/flasks/1');
+        const res = await axios.get('api/flasks/1');
+        console.log('Axios response:', res);
+        console.log('Data in useEffect:', res.data);
+        setFlask(res.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
-return (
-  <>
-    <div>
-      app page
-      changing
-      test prod
-      {JSON.stringify(flask)}
-    </div>
-  </>
-);
+  return (
+    <>
+      <div>
+        app page changing test prod
+        {JSON.stringify(flask)}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
