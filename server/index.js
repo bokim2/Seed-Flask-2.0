63a20a2c99081a65510984281fa82c9d06dcb64c
bokim2 <bokim.ce@gmail.com>
2024-01-09 19:37:53 -0800

    deployment test

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import 'dotenv/config';
+
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Define any API routes
+
+app.get('/api', (req, res) => {
+  res.send('Hello from the API');
+});
+
+// For any other route, serve the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
      app page
+     changing
     </>
   )
 }
