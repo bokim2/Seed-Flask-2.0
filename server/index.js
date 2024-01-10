240a98070474ac43a8b38927628879e43e6b8f0e
bokim2 <bokim.ce@gmail.com>
2024-01-09 20:49:41 -0800

    route test prod

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import 'dotenv/config';
 
 import path from 'path';
+import process from 'process';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -11,18 +12,38 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+import db from './db/db.js';
+
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Define any API routes
 
 app.get('/api', (req, res) => {
-  res.send('Hello from the API');
+    res.send('Hello from the API');
 });
+
+//GET one flask
+app.get("/api/flasks/:id", async(req, res)=>{
+    try{
+      const results = await db.query("select * from flasks WHERE id = $1", [req.params.id]);
+      console.log('results of getting one flask', results.rows[0]);
+      res.status(200).json({
+        status: "success",
+        results: results.rows.length,
+        data: {
+          flasks: results.rows[0]
+        }
+      })
+    } catch(err){ 
+      console.log(err)
+    }
+  })
+
 
 // For any other route, serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(PORT, () => {
