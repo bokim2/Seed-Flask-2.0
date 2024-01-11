1fbfde420a0eb923a5d189e2eeea30f8dcf7999c
bokim2 <bokim.ce@gmail.com>
2024-01-11 08:22:58 -0800

    test prod

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
     // },      
     "@typescript-eslint/no-explicit-any": 1,
     // "@typescript-eslint/no-unused-vars" : 1,
-    "no-unused-vars": "warn",
+    "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": 1,
   },
 };
