3c98dcf3bf0bf5c99d7e77cfb2b8f53cee7cc8e8
bokim2 <bokim.ce@gmail.com>
2024-01-15 14:37:47 -0800

    continuing cellbank styling

--- src/features/cellbanks/CellbanksTable.tsx
+++ src/features/cellbanks/CellbanksTable.tsx
@@ -3,9 +3,20 @@ import styled from 'styled-components';
 import CellbanksRow from './CellbanksRow';
 
 const StyledCellbanksTable = styled.table`
-  border: 1px solid green;
-  background-color: orange;
+  /* border: 1px solid green; */
+  background-color: #262231;
+  border-collapse: collapse;
+  width: 100%;
 `;
+
+const Wrapper = styled.div``;
+
+const TableContainer = styled.div`
+  max-width: 100%;
+  overflow-x: scroll;
+`;
+
+const Caption = styled.caption``;
 
 const TableHeader = styled.thead`
   background-color: orange;
@@ -15,26 +26,36 @@ const TableRow = styled.tr``;
 
 const TableHeaderCell = styled.th`
   color: lime;
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 export default function CellbanksTable({ cellbanks }) {
   console.log(cellbanks, 'in cellbankstable');
   return (
-    <StyledCellbanksTable>
-      <TableHeader>
-        <TableRow>
-          <TableHeaderCell>cell bank id</TableHeaderCell>
-          <TableHeaderCell>strain</TableHeaderCell>
-          <TableHeaderCell>target molecule</TableHeaderCell>
-          <TableHeaderCell>notes</TableHeaderCell>
-          <TableHeaderCell>details</TableHeaderCell>
-          <TableHeaderCell>date</TableHeaderCell>
-        </TableRow>
-      </TableHeader>
-      <tbody>
-        {cellbanks &&
-          cellbanks?.map((cellbank) => <CellbanksRow key={cellbank.cell_bank_id} cellbank={cellbank} />)}
-      </tbody>
-    </StyledCellbanksTable>
+    <Wrapper>
+      <TableContainer>
+        <StyledCellbanksTable>
+          <Caption>Cell Banks Table</Caption>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderCell>cell bank id</TableHeaderCell>
+              <TableHeaderCell>strain</TableHeaderCell>
+              <TableHeaderCell>target molecule</TableHeaderCell>
+              <TableHeaderCell>notes</TableHeaderCell>
+              <TableHeaderCell>details</TableHeaderCell>
+              <TableHeaderCell>date</TableHeaderCell>
+            </TableRow>
+          </TableHeader>
+          <tbody>
+            {cellbanks &&
+              cellbanks?.map((cellbank) => (
+                <CellbanksRow key={cellbank.cell_bank_id} cellbank={cellbank} />
+              ))}
+          </tbody>
+        </StyledCellbanksTable>
+      </TableContainer>
+    </Wrapper>
   );
 }
