a0bd42fc53fd40397891613ee3e5aa1352537464
bokim2 <bokim.ce@gmail.com>
2024-01-15 08:03:23 -0800

    cellbank post route completed

--- src/pages/Cellbank.tsx
+++ src/pages/Cellbank.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import CellbanksForm from '../features/cellbanks/CellbanksForm'
+import React, { useEffect, useState } from 'react';
+import CellbanksForm from '../features/cellbanks/CellbanksForm';
+import { baseUrl } from '../../configs';
 
 export default function Cellbank() {
+  const [cellbanks, setCellbanks] = useState<any>([]);
+
+  const fetchCellbanks = async () => {
+    const res = await fetch(`${baseUrl}/api/cellbanks`);
+    const {data} = await res.json();
+    console.log(data, 'in fetchcellbanks')
+    setCellbanks(data)
+    return data
+  };
+
+  useEffect(() => {
+    const data = fetchCellbanks();
+    setCellbanks(data)
+  }, []);
+
   return (
     <div>
-
-        <CellbanksForm />
+      {JSON.stringify(cellbanks)}
+      <CellbanksForm />
     </div>
-  )
+  );
 }
--- src/ui/TestComponent.tsx
+++ src/ui/TestComponent.tsx
@@ -15,14 +15,14 @@ export default function TestComponent() {
   const [flasks, setFlasks] = useState<any>([]);
 
   const [flask, setFlask] = useFlask(1)
-  console.log('flask from useFlask', flask);
+  // console.log('flask from useFlask', flask);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // console.log('in fetch data', import.meta.env.PROD);
         const {data} = await axios.get(`${baseUrl}/api/flasks`);
-        console.log('Axios response: FLASKS', data.data);
+        // console.log('Axios response: FLASKS', data.data);
 
         setFlasks(data.data);
       } catch (error) {
