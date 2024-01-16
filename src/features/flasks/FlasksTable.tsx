ff45ccd5de20a454a4249bf5b23003c5d47e0012
bokim2 <bokim.ce@gmail.com>
2024-01-15 20:11:02 -0800

    flasks table complete.  continue styling

--- src/features/flasks/FlasksTable.tsx
+++ src/features/flasks/FlasksTable.tsx
@@ -2,53 +2,49 @@ import React from 'react';
 import { useFlask, useFlasks } from '../../lib/hooks';
 import styled from 'styled-components';
 import FlasksRow from './FlasksRow';
+import {
+  Caption,
+  StyledTable,
+  TableContainer,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+  Wrapper,
+} from '../../styles/UtilStyles';
 
-const FlaskTable = styled.table`
-border: 1px solid red;
-`;
-
-const TableHeaders = styled.thead`
-color: yellow;
-`;
-
-
-const TableHeaderCell = styled.th`
-/* font-size: 0.5rem; */
-`;
-
-const TableRow = styled.tr`
-/* font-size: .5rem; */
-`;
-
-
-
-export default function FlasksTable({render}) {
-  const [flasks, isLoading] = useFlasks();
-
+export default function FlasksTable({ flasks }) {
+  console.log('flaks in flaskstable', flasks);
 
   return (
     <>
-      <FlaskTable>
-        <TableHeaders>
-          <TableRow>
-            <TableHeaderCell>Flask ID</TableHeaderCell>
-            <TableHeaderCell>Cell Bank ID</TableHeaderCell>
-            <TableHeaderCell>strain</TableHeaderCell>
-            <TableHeaderCell>target molecule</TableHeaderCell>
-            <TableHeaderCell>inoculum uL</TableHeaderCell>
-            <TableHeaderCell>media mL</TableHeaderCell>
-            <TableHeaderCell>Start date/time</TableHeaderCell>
-            <TableHeaderCell>Start date/time (pacific)</TableHeaderCell>
-            <TableHeaderCell>Start date/time (pacific -human readable)</TableHeaderCell>
-          </TableRow>
-        </TableHeaders>
-        <tbody>
-        {flasks?.map(render)
-        }
-        </tbody>
-      </FlaskTable>
-
-      <div style={{ color: 'white' }}>{JSON.stringify(flasks)}</div>
+      <Wrapper>
+        <TableContainer>
+          <StyledTable>
+            <Caption>Flasks Table</Caption>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderCell>Flask ID</TableHeaderCell>
+                <TableHeaderCell>Cell Bank ID</TableHeaderCell>
+                <TableHeaderCell>strain</TableHeaderCell>
+                <TableHeaderCell>target molecule</TableHeaderCell>
+                <TableHeaderCell>inoculum uL</TableHeaderCell>
+                <TableHeaderCell>media mL</TableHeaderCell>
+                <TableHeaderCell>Start date/time</TableHeaderCell>
+                <TableHeaderCell>Start date/time (pacific)</TableHeaderCell>
+                <TableHeaderCell>
+                  Start date/time (pacific -human readable)
+                </TableHeaderCell>
+              </TableRow>
+            </TableHeader>
+            <tbody>
+              {flasks &&
+                flasks?.map((flask) => {
+                  return <FlasksRow key={flask.flask_id} flask={flask} />;
+                })}
+            </tbody>
+          </StyledTable>
+        </TableContainer>
+      </Wrapper>
     </>
   );
 }
---
+++ src/pages/Flask.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlasksTable from '../features/flasks/FlasksTable';
+import { useFlasks } from '../lib/hooks';
+
+export default function Flask() {
+  const [flasks, isLoading] = useFlasks<any>();
+
+  return (
+    <>
+      <FlasksTable flasks={flasks} />
+    </>
+  );
+}
