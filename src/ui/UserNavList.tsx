272e28488e6ab52d7982f7312744e4d58356eaf2
bokim2 <bokim.ce@gmail.com>
2024-01-26 13:18:35 -0800

    sign-in dropdown navlist in progress

---
+++ src/ui/UserNavList.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledNavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  width: 100%;
+  background-color: rgba(var(--clr-primary-900),.7);
+z-index: 10;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  padding-right: 2rem;
+`;
+
+export default function UserNavList() {
+  return (
+    <StyledNavList>
+      <li>
+        <StyledNavLink to="/signin">Sign-In</StyledNavLink>
+      </li>
+      <li>
+        <StyledNavLink to="/cellbank">Cellbank</StyledNavLink>
+      </li>
+      <li>
+        <StyledNavLink to="/flask">Flask</StyledNavLink>
+      </li>
+      <li>
+        <StyledNavLink to="/sample">Sample</StyledNavLink>
+      </li>
+      <li>
+        <StyledNavLink to="/bioreactor">Bioreactor</StyledNavLink>
+      </li>
+    </StyledNavList>
+  );
+}
