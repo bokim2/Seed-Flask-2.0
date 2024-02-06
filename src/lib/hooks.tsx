1245519f24590603f37c8e5ebe1f3c6c35fe3fbb
bokim2 <bokim.ce@gmail.com>
2024-02-06 08:35:43 -0800

    cellbank cleanup

--- src/lib/hooks.tsx
+++ src/lib/hooks.tsx
@@ -11,7 +11,7 @@ export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 // fetch all rows in table
-useFetchQuery ()
+// useFetchQuery ()
 
 
 // Flask hooks
