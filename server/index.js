5d98153b7c73a326c678eb3d37a3202b19475f4a
bokim2 <bokim.ce@gmail.com>
2024-02-10 23:51:05 -0800

    prod github auth0 not working

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ const config = {
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
-  redirect_uri: process.env.BASE_URL + '/callback',
+  // redirect_uri: process.env.BASE_URL + '/callback',
 };
 console.log('config for auth', config);
 
