1779916d19b3c742c1c0b20d0be4bb24d7b37fef
bokim2 <bokim.ce@gmail.com>
2024-02-12 12:46:10 -0800

    post row protected

---
+++ server/middleware/allowRolesAdminUser.js
@@ -0,0 +1,25 @@
+import {prodUrl} from '../index.js'
+
+
+export const allowRolesAdminUser = (req, res, next) => {
+    console.log('req.oidc.user in cellbanks backend', req.oidc.user);
+    if (req?.oidc?.user) {
+      const userObj = req.oidc.user;
+      
+
+      const user = `${prodUrl}/roles`
+      console.log(
+       'user', user, 'userObj', userObj, 'url', prodUrl
+      )
+      const roles = userObj[user] || [];
+      if(roles.includes('admin') || roles.includes('user')) {
+        console.log('user role is admin or user')
+        next()
+      } else {
+        res.status(403).json({error: 'User is not authorized'})
+      }
+    } else {
+        res.status(403).json({error: 'User authentication is required'})
+    }
+    
+}
