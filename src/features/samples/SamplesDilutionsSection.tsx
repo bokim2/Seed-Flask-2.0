6874cd4d90e09225d7b0ee4d1957ef7f1747d08b
bokim2 <bokim.ce@gmail.com>
2024-02-27 17:58:42 -0800

    fixed dilutionCalculator function

--- src/features/samples/SamplesDilutionsSection.tsx
+++ src/features/samples/SamplesDilutionsSection.tsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import SamplesDilutionCalculator from './SamplesDilutionCalculator';
 import Button from '../../ui/Button';
 
+const initialDilutionSettings = [
+  {
+    selected: 'total',
+    selectedValue: 1000,
+    selectedCalc: 'dilutionFactor',
+    selectedCalcValue: 5,
+    rawOD600Target: 0.3,
+    rowNumber: 0,
+  },
+];
+
 export default function SamplesDilutionsSection() {
-  const [dilutionSettings, setDilutionSettings] = useState([
-    {
-      selected: 'total',
-      selectedValue: 1000,
-      selectedCalc: 'dilutionFactor',
-      selectedCalcValue: 5,
-      rawOD600Target: 0.3,
-      rowNumber: 0,
-    },
-  ]);
+  const [dilutionSettings, setDilutionSettings] = useState(
+    initialDilutionSettings
+  );
   console.log('dilutionSettings', dilutionSettings);
   return (
     <>
@@ -20,17 +24,22 @@ export default function SamplesDilutionsSection() {
         <SamplesDilutionCalculator
           key={i}
           dilutionSetting={dilutionSettings[i]}
+          dilutionSettings={dilutionSettings}
           setDilutionSettings={setDilutionSettings}
           row={i}
         />
       ))}
       <Button
         onClick={() =>
-          setDilutionSettings((prev) => [...prev, {...prev[prev.length - 1], rowNumber: prev.length}])
+          setDilutionSettings((prev) => [
+            ...prev,
+            { ...prev[prev.length - 1], rowNumber: prev.length },
+          ])
         }
       >
         Add Dilution
       </Button>
+      <Button onClick={() => setDilutionSettings(initialDilutionSettings)}>Clear Dilutions</Button>
     </>
   );
 }
