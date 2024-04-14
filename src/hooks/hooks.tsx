3212f2577acce5cf9684142f0dd5c82a79eda593
bokim2 <bokim.ce@gmail.com>
2024-04-14 10:18:06 -0700

    prod

--- src/hooks/hooks.tsx
+++ src/hooks/hooks.tsx
@@ -141,32 +141,58 @@ export function formatColumnName(columnName) {
 
 // set sorted column and asc or desc
 
+// export function useSetSortColumn<TTableColumns extends string>() {
+//   type TSortOrder = 'asc' | 'desc' | '';
+//   type TSortColumn = { [key in TTableColumns]?: TSortOrder };
+
+//   const [sortColumn, setSortColumn] = useState<TSortColumn>({});
+//   // console.log(sortColumn, 'sortColumn');
+
+//   const handleSortColumn = (e, columnName, sortOrder) => {
+//     e.stopPropagation();
+
+//     const sortObject = {
+//       [columnName]: sortOrder,
+//     };
+
+//     if (sortOrder) {
+//       setSortColumn((prev: TSortColumn) => {
+//         if (
+//           ((prev?.[columnName] === 'asc') === sortOrder) ||
+//           ((prev?.[columnName] === 'desc') === sortOrder)
+//         ) {
+//           return { [columnName]: '' };
+//         }
+//         return sortObject;
+//       });
+//     }
+//   };
+//   return { sortColumn, handleSortColumn, setSortColumn };
+// }
+
 export function useSetSortColumn<TTableColumns extends string>() {
-  type TSortOrder = 'asc' | 'desc' | '';
-  type TSortColumn = { [key in TTableColumns]?: TSortOrder };
+  type TSortOrder = 'asc' | 'desc' | ''; // Defines possible sorting states.
+  type TSortColumn = { [key in TTableColumns]?: TSortOrder }; // Mapping of column names to their sorting order.
 
   const [sortColumn, setSortColumn] = useState<TSortColumn>({});
-  // console.log(sortColumn, 'sortColumn');
 
-  const handleSortColumn = (e, columnName, sortOrder) => {
-    e.stopPropagation();
+  const handleSortColumn = (e: React.MouseEvent, columnName: TTableColumns, sortOrder: TSortOrder) => {
+    e.stopPropagation(); // Prevents the event from bubbling up the event chain.
 
-    const sortObject = {
-      [columnName]: sortOrder,
-    };
+    // Directly setting or toggling the sort state based on current and intended state.
+    setSortColumn(prev => {
+      // Current sort state of the column.
+      const currentSort = prev[columnName];
 
-    if (sortOrder) {
-      setSortColumn((prev: TSortColumn) => {
-        if (
-          ((prev?.[columnName] === 'asc') === sortOrder) ||
-          ((prev?.[columnName] === 'desc') === sortOrder)
-        ) {
-          return { [columnName]: '' };
-        }
-        return sortObject;
-      });
-    }
+      // Toggle sort state: if the same as intended, reset; otherwise, apply new sort.
+      if (currentSort === sortOrder) {
+        return { ...prev, [columnName]: '' as TSortOrder }; // Reset if the same sort order is clicked.
+      } else {
+        return { ...prev, [columnName]: sortOrder }; // Apply new sort order.
+      }
+    });
   };
+
   return { sortColumn, handleSortColumn, setSortColumn };
 }
 
